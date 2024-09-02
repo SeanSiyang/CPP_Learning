da8e79612e6f0f9af515ad92cf11e81cb5b8171f
SiyangZhang <siyang.zhang1997@outlook.com>
2024-09-02 09:20:55 +0800

    cody at home

---
+++ src/Algorithm/C2/p76.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double db1 = 4 * asin(sqrt(2.0) / 2);
+    double db2 = 3 * asin(sqrt(3.0) / 2);
+
+    if (db1 == db2)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+
+    return 0;
+}
